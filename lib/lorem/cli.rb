f2dac18e3296cf506d260ea7214566cad9207ac9
Ansuman Taria <ansuman.taria@andolasoft.co.in>
2016-07-11 16:16:12 +0530

    Changes for first gem

---
+++ lib/lorem/cli.rb
@@ -0,0 +1,12 @@
+require "thor"
+require "lorem"
+
+module Lorem
+  class CLI < Thor
+
+    desc "ipsum", "Lorem Ipsum text generator"
+      def ipsum
+        puts Lorem.ipsum
+    end
+  end
+ end
